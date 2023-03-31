26989f383bc1d3c2dc6d9f481e0027a39847e845
Guendouz mehdi <mahdign12@gmail.com>
2023-03-31 14:07:25 +0100

    work section

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import "./Work.scss"
 
 const Work = () => {
     return (
-        <div>
-            workers
+        <div className='work-container'>
+            <h1>How Do We Work Our System and Our Journey</h1>
+            <div className="item-container item-end">
+            <img src="./image/green-point.png" alt="green" className='green-point' />                
+                <div className="item first-item">
+                    <span>01</span>
+                    <div>
+                        <h4>consulting</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos blanditiis magnam incidunt sapiente animi qui quaerat ipsa corporis? Adipisci, porro?</p>
+                    </div>
+                    <img src="./icons/connect.png" alt="" />
+                </div>
+            </div>
+            <div className="item-container ">
+                <img src="./image/green-point.png" alt="green" className='green-point' />                
+                <div className="item second-item">
+                    <span>02</span>
+                    <div>
+                        <h4>Research & Complaince</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos blanditiis magnam incidunt sapiente animi qui quaerat ipsa corporis? Adipisci, porro?</p>
+                    </div>
+                    <img src="./icons/carbon.png" alt="" />
+                </div>
+            </div>
+            <div className="item-container item-end">
+                <img src="./image/green-point.png" alt="green" className='green-point' />
+                <div className="item first-item">
+                    <span>03</span>
+                    <div>
+                        <h4>Technology</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos blanditiis magnam incidunt sapiente animi qui quaerat ipsa corporis? Adipisci, porro?</p>
+                    </div>
+                    <img src="./icons/technology.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 }
